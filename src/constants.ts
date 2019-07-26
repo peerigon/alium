61bd6ad714ccf48e9131d78b4c5accb7dc007e8f
Moritz Jacobs <mail@moritzjacobs.de>
2019-07-26 14:12:38 +0200

    Make it work

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,6 @@
 /* eslint-disable import/prefer-default-export */
-export const VERSION = "1.0.0";
+export const VERSION = "0.0.1";
+export const COMMAND_FILE_NAME = ".alium/cmd";
+export const ALIASES_FILE_NAME = ".alium/aliases.json";
+export const PEC_ABORT = 66;
+export const PEC_AFTER_RUN = 23;
