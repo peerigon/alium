0a1a973b56f3bd6bc4faa857229c057105394619
Moritz Jacobs <mail@moritzjacobs.de>
2019-10-13 22:22:18 +0200

    Ignore dist/

--- dist/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/* eslint-disable import/prefer-default-export */
-exports.VERSION = "1.0.4";
-exports.COMMAND_FILE_NAME = ".alium/cmd";
-exports.ALIASES_FILE_NAME = ".alium/aliases.json";
-exports.PEC_ABORT = 66;
-exports.PEC_AFTER_RUN = 23;
-exports.ABORT_STRING = '— abort —';
